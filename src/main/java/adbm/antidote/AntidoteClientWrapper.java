44d0d4e3334f5b961eec774e6862c607ea8bedbe
alka_scaria <scaria@rhrk.uni-kl.de>
2018-02-06 14:15:12 +0100

    Antidote_Client_Wrapper updated version

--- src/main/java/adbm/antidote/AntidoteClientWrapper.java
+++ src/main/java/adbm/antidote/AntidoteClientWrapper.java
@@ -8,15 +8,19 @@ import com.google.protobuf.ByteString;
 import eu.antidotedb.antidotepb.AntidotePB;
 import eu.antidotedb.client.*;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.util.Map;
 
 import static adbm.settings.MapDBManager.getTypeOfKey;
 import static eu.antidotedb.client.Key.create;
+import static eu.antidotedb.client.Key.map_g;
 
-public class AntidoteClientWrapper extends AntidoteModel
-{
+/**
+ * Bridge between Antidote and YCSB
+ * Fetch information from Antidote
+ */
+public class AntidoteClientWrapper extends AntidoteModel {
 
     private final AntidoteClient antidote;
 
@@ -32,8 +36,13 @@ public class AntidoteClientWrapper extends AntidoteModel
 
     }
 
-    public AntidoteClientWrapper(String name)
-    {
+    /**
+     * Constructor of the class
+     *
+     * @param name
+     */
+
+    public AntidoteClientWrapper(String name) {
         //docker run -i -t -d --name antidote1 -p 8087:8087 --network antidote_ntwk -e SHORT_NAME=true -e NODE_NAME=antidote@antidote1 antidotedb/antidote
         DockerManager.runContainer(name);
         hostPort = DockerManager.getHostPortFromContainer(name);
@@ -47,24 +56,21 @@ public class AntidoteClientWrapper extends AntidoteModel
         running = true;
     }
 
-    public void stop()
-    {
+    public void stop() {
         if (running) {
             this.firePropertyChange(AntidoteController.DCListChanged, "", "");
             running = false;
         }
     }
 
-    public void start()
-    {
+    public void start() {
         if (!running) {
             this.firePropertyChange(AntidoteController.DCListChanged, "", "");
             running = false;
         }
     }
 
-    public void AddKey(String name, AntidotePB.CRDT_type type)
-    {
+    public void AddKey(String name, AntidotePB.CRDT_type type) {
         if (running) {
             AntidoteStaticTransaction tx = antidote.createStaticTransaction();
             bucket.update(tx, create(type, ByteString.copyFromUtf8(name)).reset());
@@ -73,83 +79,185 @@ public class AntidoteClientWrapper extends AntidoteModel
         }
     }
 
-    public void RemoveKey(String name){
+    public void RemoveKey(String name) {
         if (running) {
             MapDBManager.removeKey(name);
             this.firePropertyChange(AntidoteController.KeyListChanged, "", "");
         }
     }
 
-    public void ExecuteKeyOperation(String name, String operation, Object value)
-    {
+    /**
+     * To write to Antidote. Executes all the operations of antidote
+     * @param name
+     * @param operation
+     * @param value
+     */
+    public void ExecuteKeyOperation(String name, String operation, Object value) {
+
         if (running) {
             Key key = create(getTypeOfKey(name), ByteString.copyFromUtf8(name));
             UpdateOp update = null;
-            if (!operation.equals("reset")) {
-                if (operation.equals("increment") || operation.equals("decrement") || operation.equals("set")) {
-                    value = Long.parseLong(value.toString());
-                    long lvalue = (long) value;
-                    if (operation.equals("decrement")) {
-                        operation = "increment";
-                        lvalue = -lvalue;
-                    }
-                    if (operation.equals("set")) {
-                        operation = "assign";
-                    }
+            Long longObject = null;
+
+            if (key.getType().equals(AntidotePB.CRDT_type.LWWREG)) {
+                if (operation.equals("assign")) {
                     Class[] par = new Class[1];
-                    par[0] = long.class;
+                    par[0] = Object.class;
+
                     try {
                         Method method = key.getClass().getMethod(operation, par);
-                        update = (UpdateOp) method.invoke(key, lvalue);
+                        update = (UpdateOp) method.invoke(key, value);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
-                else {
-                    if (operation.equals("assign")) {
-                        Class[] par = new Class[1];
-                        par[0] = Object.class;
-                        try {
-                            Method method = key.getClass().getMethod(operation, par);
-                            update = (UpdateOp) method.invoke(key, value);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+
+            } else if (key.getType().equals(AntidotePB.CRDT_type.MVREG)) {
+                if (operation.equals("assign")) {
+                    Class[] par = new Class[1];
+                    par[0] = Object.class;
+
+                    try {
+                        Method method = key.getClass().getMethod(operation, par);
+                        update = (UpdateOp) method.invoke(key, value);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else if (operation.equals("reset")) {
+                    try {
+                        Method method = key.getClass().getMethod(operation, null);
+                        update = (UpdateOp) method.invoke(key, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if ((key.getType().equals(AntidotePB.CRDT_type.COUNTER)) || (key.getType().equals(AntidotePB.CRDT_type.FATCOUNTER))) {
+
+                CounterKey counterKey = (CounterKey) key;
+                longObject = Long.parseLong(value.toString());
+                if (operation.equals("increment")) {
+                    update = counterKey.increment(longObject);
+                } else if (operation.equals("decrement")) {
+                    update = counterKey.increment(-longObject);
+                } else if (operation.equals("reset")) {
+                    try {
+                        //Method method = key.getClass().getMethod(operation, null);
+                        update = counterKey.reset();//(UpdateOp) method.invoke(key, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (key.getType().equals(AntidotePB.CRDT_type.INTEGER)) {
+                IntegerKey integerKey = (IntegerKey) key;
+                longObject = Long.parseLong(value.toString());
+                if (operation.equals("increment")) {
+                    update = integerKey.increment(longObject);
+                } else if (operation.equals("decrement")) {
+                    update = integerKey.increment(-longObject);
+                } else if (operation.equals("reset")) {
+                    try {
+                        Method method = key.getClass().getMethod(operation, null);
+                        update = (UpdateOp) method.invoke(key, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else if (operation.equals("set")) {
+                    operation = "assign";
+
+                    Class[] par = new Class[1];
+                    par[0] = Object.class;
+
+                    try {
+                        Method method = key.getClass().getMethod(operation, par);
+                        update = (UpdateOp) method.invoke(key, value);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if ((key.getType().equals(AntidotePB.CRDT_type.GMAP)) || (key.getType().equals(AntidotePB.CRDT_type.AWMAP)) || (key.getType().equals(AntidotePB.CRDT_type.RRMAP))) {
+                MapKey gmapKey = (MapKey) key;
+
+                if (operation.equals("update")) {
+                    update = gmapKey.update(); // Check with Kevin
+                } else if (operation.equals("removeKey")) {
+                    update = gmapKey.removeKey(key);
+
+                } else if (operation.equals("removeKeys")) {
+                    // update = gmapKey.removeKeys(getMapKeyValue(gmapKey))
+
+                } else if (operation.equals("reset")) {
+                    try {
+                        Method method = key.getClass().getMethod(operation, null);
+                        update = (UpdateOp) method.invoke(key, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if ((key.getType().equals(AntidotePB.CRDT_type.ORSET)) || (key.getType().equals(AntidotePB.CRDT_type.RWSET))) {
+
+                SetKey rwsetSetKey = (SetKey) key;
+
+                if (operation.equals("add")) {
+                    update = rwsetSetKey.add(value);
+
+                } else if (operation.equals("addAll")) {
+                    //update=rwsetSetKey.addAll()
+
+                } else if (operation.equals("remove")) {
+                    update = rwsetSetKey.remove(value);
+
+                } else if (operation.equals("removeAll")) {
+
+                } else if (operation.equals("reset")) {
+                    try {
+                        Method method = key.getClass().getMethod(operation, null);
+                        update = (UpdateOp) method.invoke(key, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
             }
-            else {
-                try {
-                    Method method = key.getClass().getMethod(operation, null);
-                    update = (UpdateOp) method.invoke(key, null);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if (update != null) {
-                AntidoteStaticTransaction tx = antidote.createStaticTransaction();
-                bucket.update(tx, update);
-                tx.commitTransaction();
-            }
-            this.firePropertyChange(AntidoteController.KeyValueChanged, "", "");
+            
         }
     }
 
+    /**
+     * To read from Antidote. Execute all operations
+     * @param name
+     * @return
+     */
     // read
-    public String getKeyValue(String name)
-    {
+    public String getKeyValue(String name) {
         if (running) {
-            return bucket.read(antidote.noTransaction(), createKey(name)).toString();
+            Key key = createKey(name);
+            if (key.getType().equals(AntidotePB.CRDT_type.COUNTER) || key.getType().equals(AntidotePB.CRDT_type.FATCOUNTER) || key.getType().equals(AntidotePB.CRDT_type.INTEGER)) {
+                return bucket.read(antidote.noTransaction(), createKey(name)).toString();
+            } else if ((key.getType().equals(AntidotePB.CRDT_type.GMAP)) || (key.getType().equals(AntidotePB.CRDT_type.AWMAP)) || (key.getType().equals(AntidotePB.CRDT_type.RRMAP))) {
+                // return bucket.read(antidote.noTransaction(),)
+            } else if ((key.getType().equals(AntidotePB.CRDT_type.ORSET)) || (key.getType().equals(AntidotePB.CRDT_type.RWSET))) {
+            }
         }
         return null;
     }
 
-    private Key createKey(String name)
-    {
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public Map<String, Long> getMapKeyValue(Key<MapKey.MapReadResult> key) {
+        return bucket.read(antidote.noTransaction(), key).asJavaMap(ResponseDecoder.integer());
+    }
+
+    /**
+     *
+     * @param name
+     * @return
+     */
+    private Key createKey(String name) {
         if (running) {
             return create(getTypeOfKey(name), ByteString.copyFromUtf8(name));
         }
         return null;
     }
-
 }
